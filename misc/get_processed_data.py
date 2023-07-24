e3703bd0e6998df137df0d0bcd425b8c64ad5978
Calvin Yeung <35880052+calvin4770@users.noreply.github.com>
2023-07-24 12:07:13 -0700

    Removed unnecessary files

--- misc/get_processed_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import subprocess
-
-valid_ids = pd.read_csv("valid_ids.csv", header=None)
-valid_ids = valid_ids.to_numpy().flatten()
-
-for cell_id in valid_ids[:10]:
-    print(cell_id)

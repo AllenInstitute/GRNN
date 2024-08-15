Fixed evr computation

benchmark_pipeline.py
@@ -5,7 +5,9 @@ import sklearn.utils
 import argparse
 import os
 import json
+import torch.nn.functional as F
 
+from model import LSTM
 from train import train_model
 from evaluate import explained_variance_ratio
 from data import get_data, get_train_test_data
@@ -32,10 +34,6 @@ if save_path[-1] != '/':
 with open(args.config_path, "r") as f:
     config = json.load(f)
 
-def _lstm_predict(model, Is):
-    model.predict(Is_te[0])[0].squeeze().numpy()
-    pass
-
 def train(
     Is_tr, 
     fs_tr, 
@@ -51,7 +49,7 @@ def train(
     best_model, best_evr1, best_losses, best_test_losses = None, -1e10, [], []
     
     for i, hs in enumerate(hparams):
-        model = torch.nn.LSTM(1, hidden_size, 1, batch_first=True)
+        model = LSTM(hidden_size)
         criterion = torch.nn.PoissonNLLLoss(log_input=False, reduction="none", eps=config["eps"])
         optimizer = torch.optim.Adam(model.parameters(), lr=hs["lr"])
 
@@ -70,7 +68,7 @@ def train(
             model_type='lstm'
         )
         
-        fs_pred = _lstm_predict(model, Is_val[0])
+        fs_pred = model(Is_val[0]).detach().numpy()
         evr1 = explained_variance_ratio(fs_val[0], fs_pred, bin_size)
         
         if evr1 > best_evr1:
@@ -79,7 +77,7 @@ def train(
             best_test_losses = test_losses
             best_model = model
     
-    fs_pred = _lstm_predict(model, Is_te[0])
+    fs_pred = model(Is_te[0]).detach().numpy()
     best_evr2 = explained_variance_ratio(fs_te[0], fs_pred, bin_size)
     
     return best_model, best_evr1, best_evr2, best_losses, best_test_losses

evaluate.py
@@ -13,11 +13,11 @@ def explained_variance(psth1, psth2):
     v3 = np.std(psth1 - psth2) ** 2
     return np.nan if (v1 + v2) == 0 else (v1 + v2 - v3) / (v1 + v2)
 
-def explained_variance_ratio(model, Is_te, fs_te, bin_size, quantize=False):
+def explained_variance_ratio(fs_te, fs_pred, bin_size, quantize=False):
     fs_te_np = fs_te.numpy()
     psth_d = np.mean(fs_te_np, axis=0)
     ev_d = np.mean([explained_variance(stpsth, psth_d) for stpsth in fs_te_np])
-    psth_m = model.predict(Is_te[0])[0].squeeze().numpy()
+    psth_m = fs_pred
     if quantize:
         psth_m = quantize_prediction(psth_m, bin_size)
     pwev_dm = np.mean([explained_variance(stpsth, psth_m) for stpsth in fs_te_np])

model.py
@@ -249,5 +249,5 @@ class LSTM(torch.nn.Module):
     def forward(self, Is):
         # Is has shape [B, seq_len]
         out, _ = self.lstm(Is.unsqueeze(-1)) # [B, L, hidden_size]
-        fs_pred = F.relu(self.fc(out)).squeeze()  # [B, L, 1] so squeeze
+        fs_pred = F.relu(self.fc(out)).squeeze(dim=2)  # [B, L, 1] so squeeze
         return fs_pred